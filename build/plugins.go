e8878ccb610f158bb2573e8e5e5a8ed59255032c
NaKa2355 <24926264+NaKa2355@users.noreply.github.com>
2023-05-27 00:54:25 +0900

    fix

--- build/plugins.go
+++ build/plugins.go
@@ -7,9 +7,6 @@ when you add a structure which impliments plugin.Plugin and its name to the map,
 
 import (
 	plugin "github.com/NaKa2355/pirem/pkg/plugin/v1"
-	mockdevice "github.com/NaKa2355/pirem/third_party/mock_device"
 )
 
-var Plugins map[string]plugin.Plugin = map[string]plugin.Plugin{
-	"mock": &mockdevice.Plugin{},
-}
+var Plugins map[string]plugin.Plugin = map[string]plugin.Plugin{}
